73bd5e9dbc85ce36dea7ca7016c0c2b6585da98e
buxizhizhoum <mapeaks@126.com>
2018-04-12 15:11:10 +0800

    modify format

--- src/crop_image.py
+++ src/crop_image.py
@@ -58,8 +58,8 @@ image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # im = cv2.imread('shot.bmp')
 hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-COLOR_MIN = np.array([20, 20, 20],np.uint8)
-COLOR_MAX = np.array([255, 255, 255],np.uint8)
+COLOR_MIN = np.array([20, 20, 20], np.uint8)
+COLOR_MAX = np.array([255, 255, 255], np.uint8)
 frame_threshed = cv2.inRange(hsv_img, COLOR_MIN, COLOR_MAX)
 imgray = frame_threshed
 ret,thresh = cv2.threshold(frame_threshed,127,255,0)
@@ -68,11 +68,11 @@ img, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPRO
 # Find the index of the largest contour
 areas = [cv2.contourArea(c) for c in contours]
 max_index = np.argmax(areas)
-cnt=contours[max_index]
+cnt = contours[max_index]
 
-x,y,w,h = cv2.boundingRect(cnt)
-cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
-cv2.imshow("Show",image)
+x, y, w, h = cv2.boundingRect(cnt)
+cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+cv2.imshow("Show", image)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
