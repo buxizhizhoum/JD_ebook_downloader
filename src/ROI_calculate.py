00ac44f283057f3905d9c957a13ed32a9a8bfb27
buxizhizhoum <mapeaks@126.com>
2018-04-14 15:39:26 +0800

    test another method to find the ROI

--- src/ROI_calculate.py
+++ src/ROI_calculate.py
@@ -128,6 +128,121 @@ def calculate_roi(contour, bounding_rec_width, bounding_rec_height):
     return x, y, w, h
 
 
+def calculate_roi_2(contour):
+    """
+    a better method to find the rec of the content.
+
+    This method group point to 3 group, the points at top, the points at left
+    and the points at right.
+
+    the points at the top could be grouped by extract the point with min y,
+    after that, the left_top point and the right_top point could be extracted.
+
+    the group at left could be extracted by compare x coordinate with the point
+    at left_top, same logic to the group at right.
+
+    the left_top point is the point in the group top, and with the min x,
+    the right_top point is the point in the group right and with the max x,
+    the left_bottom point is the point in the group left and with the max y,
+    the right_bottom point is the point in the group right and with the max y
+    :param contour:
+    :return:
+    """
+    if len(contour) < 4:
+        # contour should contain at least 4 point.
+        raise ValueError("bad contour")
+
+    sorted_array = sorted(contour, key=lambda x: x[0][1])
+
+    # group of points at the top of the contour
+    group_top = []
+
+    # the points are sorted by coordinate y,
+    # the first point is belong to group_top
+    p_top_0 = sorted_array[0][0]
+    group_top.append(p_top_0)
+
+    # find points who belongs to group_top
+    for point in sorted_array:
+        point = point[0]
+        tmp_height = abs(point[1] - p_top_0[1])
+        if tmp_height < DELTA_HEIGHT_ERROR:
+            group_top.append(point)
+            # todo: if there are too many points to process, point processed
+            # should be removed from sorted_array, however, there are only
+            # several points, do not remove the point not cause heavy
+            # calculation in the following processes.
+
+    # the top left point should be the point in the group_top
+    # and with the smallest x coordinate
+    group_top.sort(key=lambda x: (x[1], x[0]))  # sort by y and x, y priority.
+    p_left_top = group_top[0]  # the point with min y and min x
+
+    # find the point with max x coordinate, that is the point at right top
+    x_max_top = p_left_top
+    for point in group_top:
+        if point[0] > x_max_top[0]:
+            x_max_top = point
+
+    p_right_top = x_max_top
+
+    # group point to 2 group according to p_left_top and p_right_top, each
+    # group should have close y coordinate, some points may be ignored
+    group_left = []
+    for point in sorted_array:
+        point = point[0]
+        diff_x = abs(point[0] - p_left_top[0])
+        if diff_x < DELTA_WIDTH_ERROR:
+            group_left.append(point)
+
+    group_left.sort(key=lambda x: x[1])
+    p_left_bottom = group_left[-1]
+
+    group_right = []
+    for point in sorted_array:
+        point = point[0]
+        diff_x = abs(point[0] - p_right_top[0])
+        if diff_x < DELTA_WIDTH_ERROR:
+            group_right.append(point)
+
+    group_right.sort(key=lambda x: x[1])
+    p_right_bottom = group_right[-1]
+
+    x, y = p_left_top[0], p_left_top[1]
+    w1 = abs(p_right_top[0] - p_left_top[0])
+    w2 = abs(p_right_bottom[0] - p_left_bottom[0])
+    # ideally they should be equal to each other
+    assert abs(w1 - w2) < DELTA_WIDTH_ERROR
+
+    h1 = abs(p_left_bottom[1] - p_left_top[1])
+    h2 = abs(p_right_bottom[1] - p_right_top[1])
+    # ideally they should be equal to each other
+    assert abs(h1 - h2) < DELTA_HEIGHT_ERROR
+
+    w = w1
+    h = h1
+
+    return x, y, w, h
+
+
+def group_maker(points, base_point, according):
+    if according in ("x", "X", 0):
+        according = 0
+    elif according in ("y", "Y", 1):
+        according = 1
+    else:
+        raise ValueError("Wrong according value, x or y")
+    # find points who belongs to group_top
+    res = []
+    for point in points:
+        point = point[0]
+        tmp_height = abs(point[according] - base_point[according])
+        if tmp_height < DELTA_HEIGHT_ERROR:
+            res.append(point)
+
+    return res
+
+
 if __name__ == "__main__":
     import numpy as np
     test_cnt = np.array([[[ 426,   1]],
@@ -146,4 +261,5 @@ if __name__ == "__main__":
                          [[1274, 1052]],
                          [[1274,    1]]])
     res = calculate_roi(test_cnt, 1079, 1078)
+    res = calculate_roi_2(test_cnt)
     print res
